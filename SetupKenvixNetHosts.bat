735b0900c98e6d39afd381570747e7e0c58567c5
Kenvix Zure <kenvixzure@live.com>
2022-02-27 18:04:28 +0800

    Add regs

---
+++ SetupKenvixNetHosts.bat
@@ -0,0 +1,3 @@
+@echo off
+SET PATH=%PATH%;C:\RT\Anaconda3R\Scripts\
+C:\RT\Anaconda3R\Scripts\hostsman.exe -i ds.kenvix.com:172.20.1.7 pass.kenvix.com:172.20.1.8 kus.kenvix.com:172.20.1.8 swrt.kenvix.com:172.20.1.1
---
+++ UnsetupKenvixNetHosts.bat
@@ -0,0 +1,3 @@
+@echo off
+SET PATH=%PATH%;C:\RT\Anaconda3R\Scripts\
+C:\RT\Anaconda3R\Scripts\hostsman.exe -r ds.kenvix.com pass.kenvix.com kus.kenvix.com swrt.kenvix.com
