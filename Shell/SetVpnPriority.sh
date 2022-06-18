569f25a9c6cb2511ddbcb2c0a7d3a05f984da87b
Kenvix Zure <kenvixzure@live.com>
2022-06-18 11:15:13 +0800

    Update SetVpnPriority.sh

--- Shell/SetVpnPriority.sh
+++ Shell/SetVpnPriority.sh
@@ -3,17 +3,35 @@
 # Kernel Software Interruption
 pgrep ksoftirqd |
 while read in; do
-    renice -n -13 -p $in
+    renice -n -14 -p $in
 done
 
 # Zerotier one
 pgrep zerotier-one |
 while read in; do
-    renice -n -13 -p $in
+    renice -n -14 -p $in
 done
 
 # WireGuard
 pgrep wg-crypt |
 while read in; do
-    renice -n -13 -p $in
+    renice -n -14 -p $in
+done
+
+# Softether VPN
+pgrep vpnserver |
+while read in; do
+    renice -n -14 -p $in
+done
+
+# Nginx
+pgrep nginx |
+while read in; do
+    renice -n -12 -p $in
+done
+
+# PHP
+pgrep php-fpm |
+while read in; do
+    renice -n -12 -p $in
 done
