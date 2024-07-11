34ad5c7e6360d6b06a43b20d7acc78c7331f6e4e
Kenvix Zure <kenvixzure@live.com>
2024-07-11 22:25:05 +0800

    Update to set network metric

--- Bat/AssignIPForWSL2-Excalibur.bat
+++ Bat/AssignIPForWSL2-Excalibur.bat
@@ -1,8 +1,23 @@
 @echo off
 title Assign IP for WSL2
 cd /d %~dp0
+echo Launching WSL2...
+wsl -d Ubuntu2 -u root ifconfig
+
+echo Setting firewall rules...
+powershell.exe -File "C:\Work-Station\Scripts\Bat\DisableWsl2Firewall.ps1"
+
+echo Assing IP for WSL2...
 wsl -d Ubuntu2 -u root ip addr add 192.0.0.2/29 broadcast 192.168.0.7 dev eth0 label eth0:1
 netsh interface ip add address "vEthernet (WSL)" 192.0.0.1 255.255.255.248
-powershell.exe -NonInteractive -NoLogo -NoProfile -File DisableWsl2Firewall.ps1
-timeout /T 5
 WSLAttachSwitch.exe --mac 9e:d5:8b:58:85:a7  Ethernet-Virt
+
+wsl -d Ubuntu2 -u root "/mnt/c/Work-Station/Scripts/Shell/WslSetNetworkMetric.sh"
+
+WSLHostPatcher.exe
+
+rem echo Disable eth0 to avoid network issues...
+rem wsl -d Ubuntu2 -u root ip link set down dev eth0
+
+echo All done !
+wsl -d Ubuntu2
